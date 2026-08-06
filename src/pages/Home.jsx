1dae7f31aa67cbafd319f313f2a6e0f2706f147d
Mattia-Giacobelli <giacobelli.mattia12@gmail.com>
2026-08-06 02:38:11 +0200

    api test

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ export default function Home() {
 
     function getRecipes() {
 
-        axios.get("http://localhost:8080/api/recipes/7")
+        axios.get("https://ricettario-spring.duckdns.org/api/recipes/7")
             .then(res => console.log(res.data))
 
     }
