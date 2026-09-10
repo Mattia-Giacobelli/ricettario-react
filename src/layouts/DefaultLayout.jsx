6682345b802812f0d2349d73d18ef6679b8132c0
Mattia-Giacobelli <giacobelli.mattia12@gmail.com>
2026-09-10 23:10:42 +0200

    fixed  winning recipe condition

--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -42,7 +42,7 @@ export default function DefaultLayout() {
                         </Link>
                     </h2>
 
-                    {lastPoll?.winningRecipe?.name != "none" && lastPoll ?
+                    {lastPoll?.winningRecipe != null ?
 
                         <div className="text-center mt-5 m-3">
 
